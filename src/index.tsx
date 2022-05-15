22e63f0819ea3c20f2ee62e9e0777834cf7bae3d
Okan Beydanol <okan.beydanol@gmail.com>
2022-05-15 14:09:53 +0300

    interface added.

--- src/index.tsx
+++ src/index.tsx
@@ -17,10 +17,62 @@ const WhatsappContactsManager = NativeModules.WhatsappContactsManager
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return WhatsappContactsManager.multiply(a, b);
-}
-
 export function getAll(): Promise<any> {
   return WhatsappContactsManager.getAll();
+}
+
+export interface EmailAddress {
+  label: string;
+  email: string;
+}
+
+export interface PhoneNumber {
+  label: string;
+  number: string;
+}
+
+export interface PostalAddress {
+  label: string;
+  formattedAddress: string;
+  street: string;
+  pobox: string;
+  neighborhood: string;
+  city: string;
+  region: string;
+  state: string;
+  postCode: string;
+  country: string;
+}
+
+export interface InstantMessageAddress {
+  username: string;
+  service: string;
+}
+
+export interface Birthday {
+  day: number;
+  month: number;
+  year: number;
+}
+
+export interface Contact {
+  recordID: string;
+  backTitle: string;
+  company: string | null;
+  emailAddresses: EmailAddress[];
+  displayName: string;
+  familyName: string;
+  givenName: string;
+  middleName: string;
+  jobTitle: string;
+  phoneNumbers: PhoneNumber[];
+  hasThumbnail: boolean;
+  thumbnailPath: string;
+  postalAddresses: PostalAddress[];
+  prefix: string;
+  suffix: string;
+  department: string;
+  birthday: Birthday;
+  imAddresses: InstantMessageAddress[];
+  note: string;
 }
